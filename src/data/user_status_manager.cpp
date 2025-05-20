ddbb2d3a6fa3b7277957cceb7fe548715eba43a0
daniel.baumann <baumann-dan@outlook.com>
2025-05-20 18:35:53 +0200

    implement response to user info query

--- src/data/user_status_manager.cpp
+++ src/data/user_status_manager.cpp
@@ -5,8 +5,14 @@
 
 llink::UserStatusManager::UserStatusManager(QSharedPointer<INetworkApi> i_network_api_ptr)
     : i_network_api_ptr_(std::move(i_network_api_ptr)) {
+    connect(i_network_api_ptr_.get(), &INetworkApi::userInfoQuery,
+            this, &UserStatusManager::respond_to_user_info_query);
 }
 
+void llink::UserStatusManager::respond_to_user_info_query(const QHostAddress &sender_address) const {
+    UserInfo user_info("Test User 1");
+    i_network_api_ptr_->sendUserInfo(sender_address, user_info);
+}
 
 void llink::UserStatusManager::send_user_info_query() const {
     qDebug() << "sending user info query";
@@ -21,4 +27,3 @@ void llink::UserStatusManager::send_user_info_broadcast() const {
 void llink::UserStatusManager::send_user_disconnect_broadcast() const {
     i_network_api_ptr_->broadcastUserDisconnected();
 }
-
--- src/data/user_status_manager.h
+++ src/data/user_status_manager.h
@@ -3,10 +3,10 @@
 #include "network_api.h"
 
 namespace llink {
-    class IUserStatusManager {
-    public:
-        virtual ~IUserStatusManager() = default;
+    class IUserStatusManager : public QObject {
+        Q_OBJECT
 
+    public:
         virtual void send_user_info_query() const = 0;
 
         virtual void send_user_info_broadcast() const = 0;
@@ -15,7 +15,11 @@ namespace llink {
     };
 
     class UserStatusManager : public IUserStatusManager {
+        Q_OBJECT
         QSharedPointer<INetworkApi> i_network_api_ptr_;
+
+    private slots:
+        void respond_to_user_info_query(const QHostAddress &sender_address) const;
 
     public:
         explicit UserStatusManager(QSharedPointer<INetworkApi> i_network_api_ptr);
